f1ae0fdb435a29345338449c06903f84696894ad
tanishjain09 <tanish.jain.0905@gmail.com>
2025-08-03 15:33:56 +0530

    router project

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './components/Home/Home'
+import Headers from './components/Header/Header'
+import Foorter from './components/Footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+    
+   </>
+  )
+}
+
+export default App
