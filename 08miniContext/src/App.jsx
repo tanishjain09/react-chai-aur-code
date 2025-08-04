84a0a61707601c36672b8472d949582dfb69ad9f
tanishjain09 <tanish.jain.0905@gmail.com>
2025-08-04 08:08:27 +0530

    context api project

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>React context api  </h1>
-    </>
+    <UserContextProvider>
+      <h1>React context api </h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
